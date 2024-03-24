72771ea749f4f912c77cbc130d68d7334bdc269b
Kendellgc1 <kgarbanzo@estudiantec.cr>
2024-03-24 16:40:21 -0600

    Creación stored procedures

---
+++ backend/database/04-storedProcedures.sql
@@ -0,0 +1,63 @@
+-- Obtener todos los usuarios y su información 
+
+DELIMITER //
+
+CREATE PROCEDURE obtenerUsuarios()
+BEGIN
+    SELECT * FROM Usuario;
+END //
+
+DELIMITER ;
+
+-- Obtener todos los proyectos y su información
+
+DELIMITER //
+
+CREATE PROCEDURE obtenerProyectos()
+BEGIN
+    SELECT * FROM Proyectos;
+END //
+
+DELIMITER ;
+
+
+
+DELIMITER //
+
+-- Cantidad de tareas en proceso, terminadas y pendientes
+
+DELIMITER //
+
+CREATE PROCEDURE tareasPorEstado()
+BEGIN
+    SELECT EstadoTarea.nombre, COUNT(Tareas.idEstado) AS Cantidad
+    FROM Tareas
+    INNER JOIN EstadoTarea ON Tareas.idEstado = EstadoTarea.idEstado
+    GROUP BY EstadoTarea.nombre;
+END //
+
+DELIMITER ;
+
+
+
+-- Registrar usuario
+
+DELIMITER //
+
+CREATE PROCEDURE registrarUsuario(
+    IN p_idProyecto INT,
+    IN p_nombre NVARCHAR(255),
+    IN p_cedula NVARCHAR(255),
+    IN p_correoElectronico NVARCHAR(255),
+    IN p_departamento NVARCHAR(255),
+    IN p_contrasena NVARCHAR(255),
+    IN p_idRol INT,
+    IN p_IdEstado INT
+)
+BEGIN
+    INSERT INTO Usuario (idProyecto, nombre, cedula, correoElectronico, departamento, contrasena, idRol, IdEstado)
+    VALUES (p_idProyecto, p_nombre, p_cedula, p_correoElectronico, p_departamento, p_contrasena, p_idRol, p_IdEstado);
+END //
+
+DELIMITER ;
+
