dda7d008e7c190a797ac8a83f9ff76c83d088ec5
ScarloVM <cvenegasm1043@gmail.com>
2024-03-25 17:02:29 -0600

    Cambio de referencias

--- frontend/www/js/scriptsCrearProy.js
+++ frontend/www/js/scriptsCrearProy.js
@@ -1,7 +1,31 @@
 //==============Pantalla Crear Proyecto-----------------------------
 
+function crearProyecto() {
+    var nombreProyecto = document.getElementById('nombreProyecto');
+    var recursos = document.getElementById('recursos');
+    var responsable = document.getElementById('responsable');
+
+
+
+    // fetch('http://localhost:3000/api/createProject', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(datos)
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     location.reload();
+    //     console.log(data);// Aquí puedes hacer algo con la respuesta del servidor si es necesario
+    // })
+    // .catch(error => {
+    //     console.error('Error:', error);
+    // });
+}
+
 function agregarColab() {
-    var selectColab = document.getElementById('colaborador').value;
+    var selectColab = document.getElementById('colaborador').value.split(",")[1];
     var listaColabs = document.getElementById('colabsEnProy');
 
     var nuevoColaborador = document.createElement('li');
@@ -11,8 +35,8 @@ function agregarColab() {
     var selectColab = document.getElementById('tarea').value = '';
 }
 
-function cargarUsuarios(ArrayUsuarios) {
-    var select = document.getElementById('colaborador')
+function cargarResponsable(ArrayUsuarios) {
+    var select = document.getElementById('responsable')
 
     ArrayUsuarios.forEach(function(usuario,index){
         var option = document.createElement('option');
@@ -22,14 +46,26 @@ function cargarUsuarios(ArrayUsuarios) {
     });
 }
 
+function cargarColaborador(ArrayUsuarios) {
+    var select = document.getElementById('colaborador')
+
+    ArrayUsuarios.forEach(function(usuario,index){
+        var option = document.createElement('option');
+        option.textContent = usuario[1];
+        option.value = [usuario[0],usuario[1]]; 
+        select.appendChild(option);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function(){
+
     fetch('http://localhost:3000/api/usersNotAsigned')
         .then(response => response.json())
         .then(data => {
             var jsonData=data[0];
 
             var ListaUsuariosSinProyecto = jsonData.map(item => [item.idUsuario, item.nombre])
-            cargarUsuarios(ListaUsuariosSinProyecto)
+            cargarColaborador(ListaUsuariosSinProyecto)
         })
 })
 
