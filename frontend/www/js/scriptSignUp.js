e43a64126ece3712bbad0a3b1cc316dd41594cc4
ScarloVM <cvenegasm1043@gmail.com>
2024-03-26 09:58:03 -0600

    Registro de Usuarios listo

--- frontend/www/js/scriptSignUp.js
+++ frontend/www/js/scriptSignUp.js
@@ -6,10 +6,30 @@ function registrarUsuario(){
     var telefono=document.getElementById('campoTelefono').value;
     var password=document.getElementById('campoPassword').value;
 
-    alert(
-        "Nombre: " + nombre + "\nCédula: " + cedula + "\nEmail: " + email + "\nDepartamento: " + departamento + "\nTeléfono: " + telefono + "\nContraseña: " + password
-    )
+    var datos={
+        nombre:nombre,
+        cedula:cedula,
+        correoElectronico:email,
+        departamento:departamento,
+        numeroTelefono:telefono,
+        contrasena:password
+    }
 
-    //Hacer el fetch para crear usuario
+    console.log(JSON.stringify(datos));
+    fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);// Aquí puedes hacer algo con la respuesta del servidor si es necesario
+        window.location.href='MenuPrincipal.html'
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 }
