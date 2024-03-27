519b4b8a44c2374980a2a7551c8e24e750507b68
ScarloVM <cvenegasm1043@gmail.com>
2024-03-27 15:55:22 -0600

    Burndown

--- frontend/www/js/burndown.js
+++ frontend/www/js/burndown.js
@@ -2,49 +2,63 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:3000/api/burndown/1') //Cambiar el id 
+        .then(response => response.json())
+        .then (data => {
+            const labels = Object.keys(data[0][0]);
+            console.log(labels)
+            const valores = Object.values(data[0][0]);
+            console.log(valores)
+
+            var data = {
+                labels: labels.reverse(),
+                datasets: [{
+                    label: 'Trabajo Restante',
+                    backgroundColor: 'blue', // Color de las barras
+                    data: valores.reverse() // Trabajo restante en cada día
+                }]
+            };
+
+            var options = {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Burndown Chart'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Días'
+                        }
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Trabajo Restante'
+                        },
+                        ticks: {
+                            beginAtZero: true // Empezar desde cero en el eje Y
+                        }
+                    }]
+                }
+            };
+
+            // Obtener el contexto del canvas
+            var ctx = document.getElementById('graficoBurndownChart').getContext('2d');
+
+            // Crear el gráfico de barras
+            var burndownChart = new Chart(ctx, {
+                type: 'line',
+                data: data,
+                options: options
+            });
+
+        })
+    
     // Datos de ejemplo para el burndown chart
-    var data = {
-        labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'],
-        datasets: [{
-            label: 'Trabajo Restante',
-            backgroundColor: 'blue', // Color de las barras
-            data: [8, 6, 4, 2, 0] // Trabajo restante en cada día
-        }]
-    };
+    
 
     // Opciones del gráfico
-    var options = {
-        responsive: true,
-        title: {
-            display: true,
-            text: 'Burndown Chart'
-        },
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Días'
-                }
-            }],
-            yAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Trabajo Restante'
-                },
-                ticks: {
-                    beginAtZero: true // Empezar desde cero en el eje Y
-                }
-            }]
-        }
-    };
-
-    // Obtener el contexto del canvas
-    var ctx = document.getElementById('graficoBurndownChart').getContext('2d');
-
-    // Crear el gráfico de barras
-    var burndownChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: options
-    });
+    
 });
