4b62128a78323ecee4474b552052a9280ed16c57
ScarloVM <cvenegasm1043@gmail.com>
2024-03-30 13:08:19 -0600

    Habilitacion de botones

--- frontend/www/js/scriptLogin.js
+++ frontend/www/js/scriptLogin.js
@@ -16,7 +16,7 @@ function iniciarLogIn() {
         contrasena:password
     };
 
-    fetch('https://api-snupie-2a6ax3i7sq-uc.a.run.app/api/login', {
+    fetch('http://localhost:3000/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
--- frontend/www/js/scriptsMenu.js
+++ frontend/www/js/scriptsMenu.js
@@ -1,12 +1,47 @@
 var parametros = new URLSearchParams(window.location.search);
 var usuarioIDEnElSistema = parametros.get("usuario");
+var rolUsuario;
 
-//==================Pantalla Menu Principal==========================
+function cargarUsuarios(ArrayUsuarios) {
+
+    ArrayUsuarios.forEach(function(usuario,index){
+        var option = document.createElement('option');
+        option.textContent = usuario[1];
+        option.value = usuario[0]; 
+        select.appendChild(option);
+    });
+}
+
+function deshabilitarBotones(){
+    var btn1 = document.getElementById('btnAsignar');
+    var btn2 = document.getElementById('btnModColab');
+    var btn3 = document.getElementById('btnCrearProyecto');
+    var btn4 = document.getElementById('btnModProyecto');
+    var btn5 = document.getElementById('btnCrearReunion');
+    var btn6 = document.getElementById('btnInformeGeneral');
+
+
+    if (rolUsuario === 2) {
+        btn1.disabled = true
+        btn1.style.backgroundColor = "#424343"
+        btn2.disabled = true
+        btn2.style.backgroundColor = "#424343"
+        btn3.disabled = true
+        btn3.style.backgroundColor = "#424343"
+        btn4.disabled = true
+        btn4.style.backgroundColor = "#424343"
+        btn5.disabled = true
+        btn5.style.backgroundColor = "#424343"
+        btn6.disabled = true
+        btn6.style.backgroundColor = "#424343"
+    }     
+}
+
 function enviarAAsignarColab() {
     window.location.href= './Asignar.html?usuario='+ encodeURIComponent(usuarioIDEnElSistema);
 }
 
-function enviarAModColab() {
+function enviarAModColab() {  
     window.location.href= './ModificarColab.html?usuario='+ encodeURIComponent(usuarioIDEnElSistema);
 }
 
@@ -33,4 +68,21 @@ function enviarAInformeGeneral() {
 function enviarAForos() {
     window.location.href= './foro.html?usuario='+ encodeURIComponent(usuarioIDEnElSistema);
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetch('http://localhost:3000/api/users')
+            .then(response => response.json())
+            .then(data => {
+                var jsonData=data;
+                console.log(jsonData);
+
+                var ListaUsuarios = jsonData.map(item => [item.idUsuario, item.idRol])
+                for (var usuario of ListaUsuarios) {
+                    if (usuario[0] === parseInt(usuarioIDEnElSistema)) {
+                        rolUsuario = usuario[1]
+                    }
+                }
+                deshabilitarBotones()
+            }); 
+})
 
